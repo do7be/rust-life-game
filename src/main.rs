e22e6450b9f87a50fd90ccbd544a0347245aeea2
do7be <bomsap700@gmail.com>
2023-07-25 18:33:28 +0900

    consoleに2秒おきに表示するところまで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let mut num = 0;
+
+    loop {
+        show_next_board(num);
+        sleep(2000);
+        num += 1;
+    }
+}
+
+fn show_next_board(num: i32) {
+    clear();
+
+    println!("Hello, world! {}", num);
+}
+
+fn sleep(millisecond: u64) {
+    use std::{thread, time};
+    thread::sleep(time::Duration::from_millis(millisecond));
+}
+
+fn clear() {
+    print!("{}[2J", 27 as char);
 }
