821391673f5961680bb1cc6e504a5561472391f1
do7be <bomsap700@gmail.com>
2023-07-26 18:38:55 +0900

    const化

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,37 @@ extern crate rand;
 use rand::Rng;
 use std::{thread, time};
 
-fn main() {
-    let length = 20;
-    let sleep_millisecond = 1000;
+const LENGTH: usize = 20;
+const SLEEP_MILLISECONDS: u64 = 1000;
 
-    let mut table: Vec<bool> = initilize_table(length);
+fn main() {
+    let mut table: Vec<bool> = initilize_table();
 
     loop {
         clear();
-        calculation_next_generation(&mut table, length);
-        show_next_board(&table, length);
-        sleep(sleep_millisecond);
+        calculation_next_generation(&mut table);
+        show_next_board(&table);
+        sleep(SLEEP_MILLISECONDS);
     }
 }
 
-fn initilize_table(length: usize) -> Vec<bool> {
-    let mut table: Vec<bool> = Vec::with_capacity(length);
+fn initilize_table() -> Vec<bool> {
+    let mut table: Vec<bool> = Vec::with_capacity(LENGTH);
     let mut rng = rand::thread_rng();
 
-    for _ in 0..(length * length) {
+    for _ in 0..(LENGTH * LENGTH) {
         table.push(rng.gen()); // 50%でtrue or false
     }
 
     table
 }
 
-fn calculation_next_generation(table: &mut [bool], length: usize) {
+fn calculation_next_generation(table: &mut [bool]) {
     let current_table = table.to_vec(); // 変更前のテーブル
 
-    for i in 0..(length * length) {
+    for i in 0..(LENGTH * LENGTH) {
         // 周囲に生きたセルがいくつあるか
-        let count = count_around_cells(current_table.to_vec(), i, length);
+        let count = count_around_cells(current_table.to_vec(), i);
         if table[i] {
             if count == 2 || count == 3 {
                 table[i] = true; // 生存
@@ -49,12 +49,12 @@ fn calculation_next_generation(table: &mut [bool], length: usize) {
     }
 }
 
-fn count_around_cells(table: Vec<bool>, index: usize, length: usize) -> i32 {
+fn count_around_cells(table: Vec<bool>, index: usize) -> i32 {
     let mut count: i32 = 0;
 
     // usizeの計算で0以下になる場合はpanicを起こすためキャストしている
     let index_i32 = index as i32;
-    let length_i32 = length as i32;
+    let length_i32 = LENGTH as i32;
 
     // 一次元で管理しているので左端、右端かどうか判定しておく必要がある
     let is_left_end = index_i32 % length_i32 == 0;
@@ -62,35 +62,35 @@ fn count_around_cells(table: Vec<bool>, index: usize, length: usize) -> i32 {
 
     // 左上
     if index_i32 - (length_i32 + 1) >= 0 && !is_left_end {
-        count += count_alive_cell(table[index - (length + 1)]);
+        count += count_alive_cell(table[index - (LENGTH + 1)]);
     }
     // 上
     if (index_i32 - length_i32) as i32 >= 0 {
-        count += count_alive_cell(table[index - length]);
+        count += count_alive_cell(table[index - LENGTH]);
     }
     // 右上
     if (index_i32 - (length_i32 - 1)) as i32 >= 0 && !is_right_end {
-        count += count_alive_cell(table[index - (length - 1)]);
+        count += count_alive_cell(table[index - (LENGTH - 1)]);
     }
     // 左
     if (index_i32 - 1) as i32 >= 0 && !is_left_end {
         count += count_alive_cell(table[index - 1]);
     }
     // 右
-    if index + 1 < length * length && !is_right_end {
+    if index + 1 < LENGTH * LENGTH && !is_right_end {
         count += count_alive_cell(table[index + 1]);
     }
     // 左下
-    if index + (length - 1) < length * length && !is_left_end {
-        count += count_alive_cell(table[index + (length - 1)]);
+    if index + (LENGTH - 1) < LENGTH * LENGTH && !is_left_end {
+        count += count_alive_cell(table[index + (LENGTH - 1)]);
     }
     // 下
-    if index + length < length * length {
-        count += count_alive_cell(table[index + length]);
+    if index + LENGTH < LENGTH * LENGTH {
+        count += count_alive_cell(table[index + LENGTH]);
     }
     // 右下
-    if index + (length + 1) < length * length && !is_right_end {
-        count += count_alive_cell(table[index + (length + 1)]);
+    if index + (LENGTH + 1) < LENGTH * LENGTH && !is_right_end {
+        count += count_alive_cell(table[index + (LENGTH + 1)]);
     }
 
     count
@@ -104,7 +104,7 @@ fn count_alive_cell(is_alive: bool) -> i32 {
     }
 }
 
-fn show_next_board(table: &[bool], length: usize) {
+fn show_next_board(table: &[bool]) {
     println!(
         "{}",
         table
@@ -112,7 +112,7 @@ fn show_next_board(table: &[bool], length: usize) {
             .enumerate()
             .fold(String::new(), |acc, (index, cell)| acc
                 + if *cell { "■" } else { "□" }
-                + if (index + 1) % length == 0 { "\n" } else { " " })
+                + if (index + 1) % LENGTH == 0 { "\n" } else { " " })
     );
 }
 
